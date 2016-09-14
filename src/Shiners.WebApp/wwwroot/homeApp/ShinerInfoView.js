32951fdc95931061ebec411fa23c157fa222b5f5
megafetis <megafetis@gmail.com>
2016-09-14 21:33:25 +0300

    update

--- src/Shiners.WebApp/wwwroot/homeApp/ShinerInfoView.js
+++ src/Shiners.WebApp/wwwroot/homeApp/ShinerInfoView.js
@@ -2,6 +2,12 @@
 import template from './ShinerInfoView.hbs.html';
 var View = Marionette.View.extend({
     
-    template:template
+    template:template,
+
+    onBeforeRender() {
+        var Handlebars = require("../../node_modules/handlebars/runtime.js");
+        var MomentHandler = require("handlebars.moment");
+        MomentHandler.registerHelpers(Handlebars);
+    }
 });
 export default View;
