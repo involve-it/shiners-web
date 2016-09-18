6e396d5b182f637fe83ba5af61c9cfb3006936d5
Евгений <xvolkx48@gmail.com>
2016-09-18 20:07:24 +0300

    Test Repository

---
+++ Shiners.Repository/Repository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+
+namespace Shiners.Repository
+{
+    public class Repository
+    {
+        private MongoClient client;
+
+        public IMongoDatabase database;
+        public ImagesStore Images { get; private set; }
+
+        public Repository()
+        {
+            string connectionString = "mongodb://localhost:3001";
+            MongoClient client = new MongoClient(connectionString);
+            db = client.GetDatabase("meteor");
+
+            Images = new ImagesStore(db);
+        }
+        
+        
+        /*
+        public void MongoDBConnect(string ConnectionString, string User, string Password)
+        {
+            var mongoUrlBuilder = new MongoUrlBuilder(ConnectionString);
+
+            try
+            {
+                client = new MongoClient(mongoUrlBuilder.ToMongoUrl());
+            }
+            catch(Exception ex) 
+            {
+
+            }
+            
+
+            //MongoCredentials credentials = new MongoCredentials(User, Password);
+            database = client.GetDatabase("meteor");
+        }
+        */
+    }
+}
---
+++ Shiners.Repository/Stores/ImagesStore.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shiners.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Bson.Serialization;
+using Shiners.Models.Domain;
+
+namespace Shiners.Repository.Stores
+{
+    public class ImagesStore
+    {
+        private static async Task<List<Image>> GetPhotos(string[] idFind)
+        {
+            
+            List<Image> ret = new List<Image>();
+            var filter = Builders<BsonDocument>.Filter.In("_id", idFind);
+            var collection = ("images");
+            using (var cursor = await collection.FindAsync(filter))
+            {
+                while (await cursor.MoveNextAsync())
+                {
+                    var images = cursor.Current;
+                    foreach (var doc in images)
+                    {
+                        ret.Add(BsonSerializer.Deserialize<Image>(doc));
+                    }
+                }
+            }
+            return ret;
+        }
+
+    }
+}
