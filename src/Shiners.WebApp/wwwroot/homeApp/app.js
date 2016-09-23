f012f44a6d2fa19d0510ebba9b9975e6071f2b8e
megafetis <fetisov@MacBook-Air-user.local>
2016-09-23 19:07:23 +0300

    fix4

--- src/Shiners.WebApp/wwwroot/homeApp/app.js
+++ src/Shiners.WebApp/wwwroot/homeApp/app.js
@@ -27,7 +27,8 @@ let App = Marionette.Application.extend({
         var rootView = new RootView({app:this});
         this.layout = rootView;
         this.showView(rootView);
-        this.getPosition();
+        //this.getPosition();
+        this.startHistory();
     },
 
     supportsHistoryApi () {
@@ -36,28 +37,28 @@ let App = Marionette.Application.extend({
 
     startHistory() {
         Backbone.history.start({pushState: this.supportsHistoryApi()});
-    },
-
-    getPosition() {
-        var app = this;
-        $.ajax({
-            dataType: "json",
-            url: 'https://www.googleapis.com/geolocation/v1/geolocate',
-            data: {key:'AIzaSyB6JoRSeKMn_yjz3Oip84N9YhX7B6djHLA'},
-            context:this
-        }).done((resp) => {
-            if (resp.location) {
-                app.position = resp.location;
-                app.startHistory();
-            } else {
-                app.startHistory();
-                console.error('geolocation not working');
-            }
-        }).fail(() => {
-            app.startHistory();
-            console.error('connection error to google geolocation api');
-        });
     }
+
+    //getPosition() {
+    //    var app = this;
+    //    $.ajax({
+    //        dataType: "json",
+    //        url: 'https://www.googleapis.com/geolocation/v1/geolocate',
+    //        data: {key:'AIzaSyB6JoRSeKMn_yjz3Oip84N9YhX7B6djHLA'},
+    //        context:this
+    //    }).done((resp) => {
+    //        if (resp.location) {
+    //            app.position = resp.location;
+    //            app.startHistory();
+    //        } else {
+    //            app.startHistory();
+    //            console.error('geolocation not working');
+    //        }
+    //    }).fail(() => {
+    //        app.startHistory();
+    //        console.error('connection error to google geolocation api');
+    //    });
+    //}
 });
 
 export default new App();
