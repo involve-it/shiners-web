ca9893b1ee71f3ad03f5401c184b363c53e1538f
arutu_000 <arutune@gmail.com>
2016-10-28 03:11:15 -0700

    work on iframe

--- src/Shiners.WebApp/wwwroot/homeApp/posts/postsMy/PostsMyView.js
+++ src/Shiners.WebApp/wwwroot/homeApp/posts/postsMy/PostsMyView.js
@@ -1,11 +1,13 @@
 ﻿import Marionette from 'backbone.marionette';
 import template from './PostsMyView.hbs.html';
+import IframeView from '../../shared/iframeView.js';
+import app from '../../app.js';
+
 var View = Marionette.View.extend({
     template:template,
-    initialize() {
-        
+ /*   initialize() {
     },
-/*
+
     tagName:'iframe',
 
     attributes: {
@@ -13,17 +15,35 @@ var View = Marionette.View.extend({
         src:'https://shiners.mobi/posts/my?isiframe=true'
     },
     */
-    events:{
-        'load iframe':'removeHeader'
-    },
+    //events:{
+    //    'load iframe':'removeHeader'
+    //},
 
-    removeHeader(e) {
-        console.info('remove header');
-        window.myIframe = e.target;
+    //removeHeader(e) {
+    //    console.info('remove header');
+    //    window.myIframe = e.target;
+    //},
+    onRender() {
+        var a = app, iView;
+        if (app.views.iframeView) {
+            iView = app.views.iframeView;
+        } else {
+            iView = new IframeView({
+                pagePath: '/posts/my'
+            });
+            iView.render();
+            app.views.iframeView = iView;
+        }
+        iView.setPage('/posts/my');
+        $('#iframeHolder').show();
     },
-
     onAttach() {
-        this.$el.height(window.innerHeight-$('#header').height());
+        $('body').addClass('sh-hidden-overflow');
+
+    },
+    onDestroy() {
+        $('#iframeHolder').hide();
+        $('body').removeClass('sh-hidden-overflow');
     }
 });
 export default View;
