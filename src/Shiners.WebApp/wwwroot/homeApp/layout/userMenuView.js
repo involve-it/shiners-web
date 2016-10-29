bc3c20079b09d0e891096a1a9add63b213e5d458
arutu_000 <arutune@gmail.com>
2016-10-28 18:15:42 -0700

    added view to show in the header (user info)

---
+++ src/Shiners.WebApp/wwwroot/homeApp/layout/userMenuView.js
@@ -0,0 +1,13 @@
+﻿
+import Backbone from 'backbone';
+import template from './userMenuView.hbs.html';
+import app from '../app.js';
+var View = Backbone.Marionette.View.extend({
+    template:template,
+    tagName: 'li',
+    events: {
+        
+    }
+
+});
+export default View;
