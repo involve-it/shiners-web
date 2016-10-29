fe03ee5a44162f3cff93e9ab58269861d6b51f45
arutu_000 <arutune@gmail.com>
2016-10-28 18:13:18 -0700

    added _toggleUserInfo in the header, _adaptUiIfIsIframe if isiframe=true querystring is provided, moved _loadIframeView here from app.js

--- src/Shiners.WebApp/wwwroot/homeApp/mainLayoutView.js
+++ src/Shiners.WebApp/wwwroot/homeApp/mainLayoutView.js
@@ -1,20 +1,24 @@
-﻿import Marionette from 'backbone.marionette';
-import template from './MainLayoutView.hbs.html';
+﻿import Backbone from 'backbone';
+import Marionette from 'backbone.marionette';
+import template from './mainLayoutView.hbs.html';
 import _ from 'underscore';
 import MapView from './MapView.js';
 import BannerView from './BannerView.js';
 import UserBarView from './UserBarView.js';
 import NavLocationView from './NavLocationView.js';
 import app from './app.js';
+import IframeView from './shared/iframeView.js';
+import UserMenuView from './layout/userMenuView';
 
 var View = Marionette.View.extend({
 
     template:template,
-
     id:"wrapper",
 
     mapView:null,
-
+    initialize() {
+        
+    },
     regions: {
         'content':'#appContent',
         'map':'#appMap',
@@ -22,7 +26,6 @@ var View = Marionette.View.extend({
         'userBar':'#userBar',
         'navLocation':'#navLocation'
     },
-
     onClickLink() {
         Backbone.$(document).on('click', 'a:not([data-direct-link])', function (e) {
             var href = Backbone.$(this).attr('href');
@@ -37,6 +40,10 @@ var View = Marionette.View.extend({
     onRender() {
         this.renderMapAndBanner();
         this.listenTo(app.router,'route',this.toggleMapAndBanner);
+        this._adaptUiIfIsIframe();
+
+        this.listenTo(app.user, 'receivedMeteorUser', this._toggleUserInfo);
+        this._loadIframeView();
     },
 
     onAttach() {
@@ -84,6 +91,43 @@ var View = Marionette.View.extend({
     },
     showFooter() {
         this.$('#footer').show();
+    },
+    _toggleUserInfo(user) {
+        var view = new UserMenuView({ 
+            model: new Backbone.Model(user)
+        });
+        view.render();
+        this.$('#userBar .quick-cart').hide();
+        this.$('#userBar ul').append(view.$el);
+        this.$('#userBar').show();
+    },
+    _adaptUiIfIsIframe() {
+        var isIframe = $h.help.getUrlParams().isiframe;
+        if (isIframe === 'true') {
+            this.$('#header').hide();
+            this.hideFooter();
+            // hide all post-beginning links (we don't want user to go somewhere)
+            setTimeout(() => {
+                $('a[href^="/posts/"]').each((i, a) => {
+                    $(a).hide()
+                });
+            }, 1000);
+        }
+    },
+    _loadIframeView() {
+        var that = this;
+        setTimeout(()=> {
+            if (!app.views.iframeView) {
+                var iView = new IframeView({
+                    //pagePath: 'posts/new'
+                });
+                iView.render();
+                app.views.iframeView = iView;
+            }
+        }, 2000); // let's assume main page is loaded by this time
+
+    },
+    onAttach() {
     }
 });
 export default View;
