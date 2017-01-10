955ac033b28e1ae7a3805fc6be6d591c45e14d68
megafetis <megafetis@gmail.com>
2017-01-10 17:21:49 +0300

    distanse

--- src/Shiners.WebApp/wwwroot/homeApp/search/ShinersListItemView.js
+++ src/Shiners.WebApp/wwwroot/homeApp/search/ShinersListItemView.js
@@ -1,5 +1,7 @@
 ﻿import Marionette from 'backbone.marionette';
 import template from './ShinersListItemView.hbs.html';
+import app from '../app'
+import locationHelper from '../../helpers/locationHelper.js'
 var View = Marionette.View.extend({
     className:'sh-shiners-list-item clearfix',
     tagName:'div',
@@ -7,6 +9,27 @@ var View = Marionette.View.extend({
 
     initialize() {
         
+    },
+
+    onBeforeRender() {
+        this.setModelDistance();
+    },
+
+    setModelDistance() {
+        var locations = this.model.get('details').locations;
+        if (locations && _.size(locations) > 0 && app.user.has('position')) {
+            var location = _.find(locations, function(l) { return l.placeType === 'dynamic'; });
+            if (!location)
+                location = locations[0];
+            var dist = locationHelper.getDistance(location.coords.lat,
+                location.coords.lng,
+                app.user.get('position').lat,
+                app.user.get('position').lng);
+            this.model.set('distance',dist );
+            this.model.set('distanceType', 'km');
+        } else {
+            this.model.set('distance',-1);
+        }       
     }
 });
 export default View;
