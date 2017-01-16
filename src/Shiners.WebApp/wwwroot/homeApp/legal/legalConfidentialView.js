4a41f2c0765870ac4becd3014b1b075ba61525d4
arutu_000 <arutune@gmail.com>
2017-01-15 23:30:57 -0800

    work on Legal pages

---
+++ src/Shiners.WebApp/wwwroot/homeApp/legal/legalConfidentialView.js
@@ -0,0 +1,13 @@
+/**
+ * Created by arutu_000 on 1/15/2017.
+ */
+import Marionette from 'backbone.marionette';
+import template from './legalConfidentialView.hbs.html';
+
+var View = Marionette.View.extend({
+    template:template,
+    onRender() {
+        debugger;
+    }
+});
+export default View;
---
+++ src/Shiners.WebApp/wwwroot/homeApp/legal/legalUserAgreementView.js
@@ -0,0 +1,10 @@
+/**
+ * Created by arutu_000 on 1/15/2017.
+ */
+import Marionette from 'backbone.marionette';
+import template from './legalUserAgreementView.hbs.html';
+
+var View = Marionette.View.extend({
+    template:template
+});
+export default View;
