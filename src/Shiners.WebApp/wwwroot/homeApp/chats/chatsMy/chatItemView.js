deaae610f23c014a5e513a98ceca3496121375d9
arutu_000 <arutune@gmail.com>
2017-01-15 22:18:23 -0800

    work on my chats & chat pages

---
+++ src/Shiners.WebApp/wwwroot/homeApp/chats/chatsMy/chatItemView.js
@@ -0,0 +1,23 @@
+﻿import Marionette from 'backbone.marionette';
+import template from './chatItemView.hbs.html';
+import app from '../../app.js';
+import './chatItemView.css';
+
+var View = Marionette.View.extend({
+    template:template,
+    onBeforeRender() {
+        this.getDistance();
+    },
+    getDistance() {
+        // todo
+    },
+    events: {
+        '.js-remove-my-post click': ()=>{
+            //debugger;
+        },
+        '.js-edit-my-post click': ()=>{
+            //debugger;
+        }
+    }
+});
+export default View;
