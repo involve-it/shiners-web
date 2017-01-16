f4d683f5b349b8b3e867625a8d0bd9f62aece504
arutu_000 <arutune@gmail.com>
2017-01-15 22:38:44 -0800

    work on my chats & chat pages

---
+++ src/Shiners.WebApp/wwwroot/homeApp/chats/chatsMy/chatsMyView.js
@@ -0,0 +1,11 @@
+﻿import Marionette from 'backbone.marionette';
+import template from './chatsMyView.hbs.html';
+import ItemView from './chatItemView'
+var View = Marionette.CompositeView.extend({
+    template: template,
+    childView: ItemView,
+    childViewContainer: '#myChatsContaner',
+    onRender() {
+    }
+});
+export default View;
