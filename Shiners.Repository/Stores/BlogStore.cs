89db6cff0b557d30caadcfaa5a71ed78d80564b6
arutu_000 <arutune@gmail.com>
2017-01-25 10:37:23 -0800

    added blog.cs files

---
+++ Shiners.Repository/Stores/BlogStore.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using Shiners.Models.Domain;
+
+namespace Shiners.Repository.Stores
+{
+    public class BlogsStore:BaseStore<Post>
+    {
+        public BlogsStore(IMongoDatabase db, string collectionName) : base(db, collectionName)
+        {
+        }
+
+        public async Task<IList<Post>>  GetPosts(string userId)
+        {
+            return await this.GetCollection();
+        }
+
+        public async Task<bool> GetPostById(string postId)
+        {
+            var result = await this.Get(p => p.Id == postId);
+            return result != null;
+        }
+
+    }
+}
