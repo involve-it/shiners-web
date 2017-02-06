09d62b130a334cd85efe762def4de3ffd42a2371
arutu_000 <arutune@gmail.com>
2017-02-05 18:50:16 -0800

    added bootstrap-override

--- src/Shiners.WebApp/wwwroot/index.js
+++ src/Shiners.WebApp/wwwroot/index.js
@@ -6,6 +6,7 @@ import './helpers/locationHelper';
 import './lib/bootstrap/dist/js/bootstrap.min.js';
 import './lib/jquery-ui-only-slider/jquery-ui.min.js';
 import moment from 'moment';
+import './css/bootstrap-override.less';
 
 import app from './homeApp/app.js'
 window.serverRender = () => {};
