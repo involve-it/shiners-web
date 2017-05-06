dc798af830d134d934af7f145777b7d81ad6894c
arutu_000 <arutune@gmail.com>
2017-05-05 22:27:38 -0700

    work on app page

---
+++ src/Shiners.WebApp/Controllers/AppController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.S3;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace Shiners.WebApp.Controllers
+{
+    //[RequireHttps]
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        { 
+            return View();
+        }
+    }
+}
