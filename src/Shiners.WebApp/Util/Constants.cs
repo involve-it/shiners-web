266a740730783ce479b3cae260801f2936e603cb
Yury <ydorofeev@superiorvision.com>
2017-05-31 18:30:49 -0700

    bundle.js version number

---
+++ src/Shiners.WebApp/Util/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shiners.WebApp.Util
+{
+    public class Constants
+    {
+        public static readonly string BUILD_ID = Guid.NewGuid().ToString();
+    }
+}
